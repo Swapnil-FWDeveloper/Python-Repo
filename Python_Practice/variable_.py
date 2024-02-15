7e03f1114a78fc3128300f2a77350f3ea752a972
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-02-15 23:58:39 +0530

    New file

--- Python_Practice/variable_.py
+++ Python_Practice/variable_.py
@@ -66,4 +66,13 @@ print(e)
 
 
 # Rule 5
+# Result o fdivision operator with two integers will be float
+A,B=1,2
+C=A/B
+print(C)
 
+Rule 6
+# Integer division with float and int will give int displayed as float
+num1,num2=1.5,3
+num3=num1//num2
+print(num3,num1.num2)
