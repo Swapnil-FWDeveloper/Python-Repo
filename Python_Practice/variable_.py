4142f91acdc116204f9979529afdbb1f72d1caaf
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-02-16 14:21:40 +0530

    New file

--- Python_Practice/variable_.py
+++ Python_Practice/variable_.py
@@ -1,69 +1,89 @@
-# Print "Hello world"
-print("Hello world")
+# # Print "Hello world"
+# print("Hello world")
 
-# Print "Hello my name is Swapnil" in one line
-print("Hello", "my name is Swapnil")
+# # Print "Hello my name is Swapnil" in one line
+# print("Hello", "my name is Swapnil")
 
-# Define and print name
-name = "Swapnil"
-print(name)
+# # Define and print name
+# name = "Swapnil"
+# print(name)
 
-# Reassign the name variable
-name = 'Raja'  # Reassigning
-name = 'Swapnil'  # Reassigned
-print(name)
+# # Reassign the name variable
+# name = 'Raja'  # Reassigning
+# name = 'Swapnil'  # Reassigned
+# print(name)
 
-# Define variables with different data types and print them
-age = 25  # Integer
-name = "John"  # String
-height = 5.11  # Float
-is_student = True  # Boolean
-no_value = None  # None
+# # Define variables with different data types and print them
+# age = 25  # Integer
+# name = "John"  # String
+# height = 5.11  # Float
+# is_student = True  # Boolean
+# no_value = None  # None
 
-print(age)
-print(name)
-print(height)
-print(is_student)
-print(no_value)
+# print(age)
+# print(name)
+# print(height)
+# print(is_student)
+# print(no_value)
 
-# Print the sum of two variables
-a = 2
-b = 5
-sum_ab = a + b
-print(sum_ab)
+# # Print the sum of two variables
+# a = 2
+# b = 5
+# sum_ab = a + b
+# print(sum_ab)
 
-# Expression Execution Rule 1
-A, B = 2, 3
-expExe = "@"
-print(A * expExe * B)
-print(2 * expExe * 3)
+# # Expression Execution Rule 1
+# A, B = 2, 3
+# expExe = "@"
+# print(A * expExe * B)
+# print(2 * expExe * 3)
 
-# Expression Execution Rule 2
-# String and string can operate with +
-a, b = "2", 3
-expExe1 = "@"
-print((a + expExe1) * b)
+# # Expression Execution Rule 2
+# # String and string can operate with +
+# a, b = "2", 3
+# expExe1 = "@"
+# print((a + expExe1) * b)
 
-# Expression Execution Rule 3
-# Numeric values can operate with all the operators
-c, d = 2, 3
-e = 4
-print(c + d * e)
+# # Expression Execution Rule 3
+# # Numeric values can operate with all the operators
+# c, d = 2, 3
+# e = 4
+# print(c + d * e)
 
-# Expression Execution Rule 4
-# Arithmetic expression with Integer and float will result in float
-c, d = 2.5, 3
-e = c * d
-print(e)
+# # Expression Execution Rule 4
+# # Arithmetic expression with Integer and float will result in float
+# c, d = 2.5, 3
+# e = c * d
+# print(e)
 
-# Expression Execution Rule 5
-# Result of division operator with two integers will be a float
-A, B = 1, 2
-C = A / B
-print(C)
+# # Expression Execution Rule 5
+# # Result of division operator with two integers will be a float
+# A, B = 1, 2
+# C = A / B
+# print(C)
 
-# Expression Execution Rule 6
-# Integer division with float and int will give int displayed as float
-num1, num2 = 1.5, 3
-num3 = num1 // num2
-print(num3, num1 / num2)
+# # Expression Execution Rule 6
+# # Integer division with float and int will give int displayed as float
+# num1, num2 = 1.5, 3
+# num3 = num1 // num2
+# print(num3, num1 / num2)
+
+
+# # Input in Python
+# # intput() statement is uesd to accept values (using kyeboard) from user
+
+# # string input
+# name = input("name : ")
+# # int input
+# age=int(input("age : "))
+# # float input
+# price=float(input("price : "))
+
+#Taking input from user & printing it
+
+name=input("name : ")
+age=int(input("age : "))
+roll_no=float(input("Roll. No : "))
+
+print("My name is ", name , "and my age is",age,"and my roll no is", roll_no)
+
