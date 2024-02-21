9a41366e6a9025d571e41f0a5a53f7ccde293ecc
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-02-21 18:23:40 +0530

    code refacor

--- Python_Practice/strings_.py
+++ Python_Practice/strings_.py
@@ -34,5 +34,34 @@
 # Practice
 # WAP to input user's first name and print its length
 
-Enter_Name=str(input("Enter your name : "))
-print(Enter_Name,"name length is ", len(Enter_Name))
+# Enter_Name = str(input("Enter your name : "))
+# print(Enter_Name,"name length is ", len(Enter_Name))
+
+
+# Conditional Statement
+#if-elif-else (syntax)
+
+# Example Result Day
+
+Eng_Marks = int(input("Enter your English marks : "))
+Maths_Marks = int(input("Enter your Maths marks : "))
+Science_Marks = int(input("Enter your Science marks : "))
+Hindi_Marks = int(input("Enter your Hindi marks : "))
+Social_Science = int(input("Enter your Social Science marks : "))
+
+Best_5 = Eng_Marks + Maths_Marks + Science_Marks + Hindi_Marks + Social_Science
+Percentage = (Best_5 / 500) * 100
+print(Percentage)
+
+if (Percentage >= 92) :
+    print("Congratulation you got A+")
+elif (Percentage >= 80 < 91) :
+    print("Congratulation you got A")
+elif (Percentage >= 70 < 80) :
+    print("Congratulation you got B+")
+elif (Percentage >= 60 < 70) :
+    print("Congratulation you got B")
+elif (Percentage >= 50 < 60) :
+    print("Congratulation you got C+")            
+else : 
+    print("No Worry work on your skills")
