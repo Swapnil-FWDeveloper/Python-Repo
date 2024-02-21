a4cbc6182b3c12794b384fceb12a399ce7c1b8c9
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-02-21 17:12:28 +0530

    code refactoring

--- Python_Practice/strings_.py
+++ Python_Practice/strings_.py
@@ -1,2 +1,34 @@
 # Strings 
 # String is data type that stores a sequence of characters
+
+# Slicing
+
+# str="Apna Collage"
+# print(str[1:3])
+
+# String Function
+
+# replace()
+# When ever we want to replace string character with another character then we use String replace funciton
+
+
+# Replace_Char = "Hello my Name is Swapnil";
+# print(Replace_Char.replace("H", "B"))
+# print(Replace_Char.replace("Hello", "Volla"))
+
+# We can replace entire Sentence also
+
+# find()
+# Used in finding character in a string with its index
+
+# Find_Char = "Hello my Name is Swapnil";
+# print(Find_Char.find("Name"))
+
+# count()
+# Count funciton counts the occurances of character
+
+# Count_Char = "Hello my Name is Swapnil and I am Data Scientist";
+# print(Count_Char.count("a"))
+# print(Count_Char.count("Hello", "Volla"))
+
+Practice
