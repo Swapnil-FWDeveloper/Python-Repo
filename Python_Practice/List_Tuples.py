a46e92fea4ad2cb1d7c9890ed01c40b5b4be3003
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-02-22 12:53:26 +0530

    New Topic added

---
+++ Python_Practice/List_Tuples.py
@@ -0,0 +1,6 @@
+# Lists in Python
+# A build-in data type that stores set of values 
+# It can store elements of different types (integer, float, string, etc.)
+# Lists are the data type where same or diffrent type of elements can be stored in memory
+# Lists = Array
+marks = [87,64,33,95,76]
