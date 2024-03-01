5d8686dee3f22344ceda16506436100c36ef927b
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-03-01 20:05:39 +0530

    refactor Adding new Jypyter loopPracticeset's files

--- Python_Practice/Loop_/For_Loop/Loop_.py
+++ Python_Practice/Loop_/For_Loop/Loop_.py
@@ -10,14 +10,14 @@
 # else:
     # work when loops ends
 
-# list=[1,2,3,4,5]
-# for el in list :
-#     print(list)
+list=[1,2,3,4,5]
+for el in list :
+    print(list)
 
 # use in tuple
-# tup=(1,2,3,4,5,6,7,8,9)
-# for i in tup :
-#     print(i)
+tup=(1,2,3,4,5,6,7,8,9)
+for i in tup :
+    print(i)
 
 # range()
 # Range funciton returns a sequence of numbers, starting from 0 by default and increments by 1 (by default), and stops before a specified number
@@ -27,18 +27,18 @@
 # The line `for i in list(5)` is attempting to iterate over the list variable, but it seems to have a
 # syntax error. The correct syntax should be `for i in list:` to iterate over the elements in the
 # list. The `(5)` after `list` is incorrect and should be removed.
-# for i in range(2,100,2) :
-#     print(i)
+for i in range(2,100,2) :
+    print(i)
     # here loop is started from 2 till 100 with diffrence of 2
 
 
 #Practice
 # Run a loop form 1 to 100
-# for i in range(1, 100) :
-#     print(i)
+for i in range(1, 100) :
+    print(i)
 
-# for i in range(100, 0, -1) :
-#     print(i)
+for i in range(100, 0, -1) :
+    print(i)
 
 #  print the table 
 n=int(input("enter the number : "))
---
+++ Python_Practice/Loop_/For_Loop/question_.py
@@ -0,0 +1,22 @@
+
+# Basic Level:
+# a. Write a Python program to print all numbers from 1 to 10 using a while loop.
+# b. Write a Python program to find the sum of all numbers from 1 to 100 using a for loop.
+# c. Write a Python program to print the multiplication table of a given number using a while loop.
+# d. Write a Python program to print the elements of a list using a for loop.
+
+# 1
+
+count=11
+i=1
+while i < count :
+    print(i)
+    i+=1
+
+# 2
+sum=0 
+el=100   
+for sum in el :
+    sum+=el
+print(sum)
+    
