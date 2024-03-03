1d3e5fbea356bb2ce1ba8c849274287c1697b5dc
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2024-03-03 18:02:51 +0530

    Refactor adding function file

---
+++ Python_Practice/Functions_/functions_.py
@@ -0,0 +1,13 @@
+# Function
+# Function is a statement that perform a specific task
+
+# Syntaax def function_name(parameter) : 
+# some work
+# return value or print value
+# function_name(argument1, argument2) function call
+
+
+def sum(a,b) :
+    s=a+b
+    return s
+print(sum(5,2))
